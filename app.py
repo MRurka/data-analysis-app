2f8c3b3e2bbc10759ac1a86d09f824340fd77732
Michael Rurka <michael@rurkadesign.com>
2022-03-30 18:15:11 -0500

    Spruced up the login page and some baseline bootstrap styles.

--- app.py
+++ app.py
@@ -12,22 +12,33 @@ app = Dash(
 
 app.layout = html.Div([
     html.Div(id='page-content'),
-    boot.Container ([
-        boot.Row(
+    boot.Container ([   
+        boot.Row([
             boot.Col([
-                dcc.Input(id = "login-email", type = "email", placeholder = "email"),
-                dcc.Input(id = "login-password", type = "password", placeholder = "password",),
-                html.Button('Login', id = 'login-button'),
-            ]),
-        ),
-    ], id = "login-content")
+                html.Div([
+                    html.Img(src="assets/logo.svg")
+                ], className = "login-logo"),
+                html.Div([
+                    boot.Label("Email", html_for="login-email"),
+                    boot.Input(id = "login-email", type = "email", placeholder = "", autoFocus = "autofocus")
+                ], className = "mb-3"),
+                html.Div([
+                    boot.Label("Password", html_for="login-password"),
+                    boot.Input(id = "login-password", type = "password", placeholder = "")
+                ], className = "mb-3"),
+                boot.Button('Login', id = "login-button", color = "primary", className = "btn-full-width"),
+                boot.FormText(id = "login-response"),
+            ], width = 3 ), # Col
+        ], justify="center" ), # Row
+    ], id = "login-content", className = "login-page") #Container
 ])
 
-# Login
+# Login: Successful
 #
 @callback(
     Output('page-content', 'children'),
     Output('login-content', 'children'),
+    Output('login-response', 'children'),
     Input('login-email', 'value'),
     Input('login-password', 'value'),
     Input('login-button', 'n_clicks')
@@ -37,9 +48,10 @@ def login_user(email, password, n_clicks) :
         try : 
             auth.sign_in_with_email_and_password(email, password)
             print("Login successful")
-            return dashboard.layout, " " # Return dashboard and hide login content
+            return dashboard.layout, " ", " " # Return dashboard and hide login content
         except :
             print("Login failed")
+            return " ", app.layout, "Your email or password is incorrect. Please try again :)"
 
 # Run Server
 #
