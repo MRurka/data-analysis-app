4610deb9418816cb0ef9f9e2844575e66245f7a2
Michael Rurka <>
2022-05-17 13:26:24 -0400

    Data file tidy up, and created function for creating areas dataframe (although it's not needed, at the moment)

--- data.py
+++ data.py
@@ -4,27 +4,25 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import pandas as pd
 
-# from authentication import auth
-
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
-
 def get_data(collection, field, user_uid):
     
-    # GET DATA
+    # Get Data from Firebase
     db = firestore.client()
     life_areas = db.collection(collection).where(field, '==', user_uid).stream()
 
-    # CREATE DATAFRAME<
+    # Create dataframe
     df = pd.DataFrame()
 
-    # FILL DATAFRAME
+    # Fill the dataframe
     for entry in life_areas:
         new_entry = entry.to_dict()
         new_row = pd.DataFrame.from_records(new_entry, index=[0])
         df = pd.concat([df, new_row], axis=0)
     
+    # Replace string with numbers
     df = df.replace({
         'Bad'  : 1,
         'Meh'  : 2,
@@ -33,34 +31,22 @@ def get_data(collection, field, user_uid):
         'Great': 5
     })
 
+    # Format date
     df['creation_date'] = pd.to_datetime(df['creation_date'], format='%A, %B %d, %Y at %I:%M %p')
 
+    # Reset the dataframes Index
     df = df.reset_index(drop=True)
 
     return df
 
 
-def df_convert_to_datetime(dataframe, column_name):
-    dataframe[column_name] = pd.to_datetime(df['Date'], format='%A, %B %d, %Y at %I:%M %p')
-    return dataframe
+def create_areas_df(dataframe):
 
+    # Create dataframe
+    df_areas = []
 
-def convert_moods_to_numbers(dataframe):
-    dataframe = dataframe.replace({
-    'Bad'  : 1,
-    'Meh'  : 2,
-    'Okay' : 3,
-    'Good' : 4,
-    'Great': 5
-    })
-    return dataframe
+    # Fill dataframe with area columns
+    for i in dataframe.loc[:,'area_score_career' : 'area_score_social']:
+        df_areas.append(i)
 
-
-# Dataframe for Areas
-# df_areas = []
-# for i in df.loc[:,'H - Health Phys' : 'H - Life Overall']:
-#     df_areas.append(i)
-
-# print(df)
-
-
+    return df_areas
